24bc93676ca2cc0f34c58214bf7f0242c8e70c81
akshayabathala <akshayabathala@gmail.com>
2025-07-05 17:30:32 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ dotenv.config()
 
 const app = express()
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://virtualassistant-uoli.onrender.com",
     credentials:true
 }))
 const port = process.env.port || 5000
